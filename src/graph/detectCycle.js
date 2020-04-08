123fb1d18e3d7e680d048b42a2ea2ae55f1c4e45
Alexandr Chernyakov <uptech@Alexs-MacBook-Pro.local>
2020-04-08 22:55:56 +0300

    add detectCycle method to graph

---
+++ src/graph/detectCycle.js
@@ -0,0 +1,27 @@
+import Stack from '../stack'
+
+export default function detectCycle(graph, source = 0) {
+  const numOfVertices = graph.vertices;
+  const visited = new Set()
+  const stack = new Stack(numOfVertices);
+  for(let i = source; i < numOfVertices; i++){
+    if(!visited.has(i)){
+      stack.push(i)
+      while(!stack.isEmpty()){
+        const activeIdx = stack.pop()
+        visited.add(activeIdx)
+        let node = graph.list[activeIdx].getHead()
+        while(node){
+          if(!visited.has(node.data)){
+						stack.push(node.data)
+						node = node.nextElement
+					}else{
+						return true
+					}
+          
+        }
+      }
+    }
+  }
+  return false
+}
